4edae7f3b873b0c56d6b242e00ccd6d649427e33
Abdikani Ahmed <cabdiqanisoomaal@gmail.com>
2021-01-06 10:30:05 +0300

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
+import  Menu  from "./components/MenuComponent";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color="primary">
+      <Navbar dark color="primary" className="navbar-fix">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
        </Navbar>
+       <Menu />
     </div>
   );
 }
